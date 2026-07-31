e293409c137bae0a8461af9a6283c23a5140f6d5
belamard kiala <belamard@gmail.com>
2026-07-31 12:58:21 +0100

    first commit

---
+++ src/main/java/com/b2la/antiplagiat/AntiplagiatApplication.java
@@ -0,0 +1,13 @@
+package com.b2la.antiplagiat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiplagiatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiplagiatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/b2la/antiplagiat/AntiplagiatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2la.antiplagiat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiplagiatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
